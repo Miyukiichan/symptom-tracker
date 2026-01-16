11822708aabca4f5e8a7f13eb598262bb38e630b
Sam Matthews <sam.matthews19638@gmail.com>
2026-01-16 09:30:02 +0000

    Streamline setup + add actions + report function

--- src/Program.cs
+++ src/Program.cs
@@ -6,19 +6,14 @@ var configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolde
 var dataFileName = "data.json";
 var notesPath = Path.Combine(configPath, "notes");
 var dataPath = Path.Combine(configPath, dataFileName);
-if (!Directory.Exists(configPath)) {
-	Console.WriteLine("Would you like to go through first time setup? (y/n)");
-	var a = Console.ReadLine();
-	if (a is null || a.ToLower() != "y")
-		return;
+if (!Directory.Exists(configPath))
 	Directory.CreateDirectory(configPath);
+if (!File.Exists(dataPath))
 	File.Copy(dataFileName, dataPath);
-}
 if (!Directory.Exists(notesPath))
 	Directory.CreateDirectory(notesPath);
 
 // Read database
-var date = DateTime.Today.ToString("yyyy-MM-dd");
 var data = JsonConvert.DeserializeObject<Data>(File.ReadAllText(dataPath));
 if (data is null)
 	throw new Exception("Error processing data file");
@@ -30,11 +25,14 @@ foreach(var s in data.Symptoms) {
 }
 
 // Initial user prompt
+var date = DateTime.Today.ToString("yyyy-MM-dd");
 Console.WriteLine($"Hello {data.Name}, today's date is {date}. What would you like to do?\n");
 Console.WriteLine($"1. Track my symptoms");
 Console.WriteLine($"2. See today's report");
 Console.WriteLine($"3. Edit daily note");
 Console.WriteLine($"4. See report from a previous day");
+Console.WriteLine($"5. My Symptoms");
+Console.WriteLine($"6. My Suggested Actions");
 var answer = string.Empty;
 var answerNumber = -1;
 while (string.IsNullOrWhiteSpace(answer)) {
@@ -44,7 +42,7 @@ while (string.IsNullOrWhiteSpace(answer)) {
 		answer = string.Empty;
 		continue;
 	}
-	if (answerNumber != 1 && answerNumber != 2 && answerNumber != 3 && answerNumber != 4) {
+	if (answerNumber < 1 || answerNumber > 6) {
 		Console.WriteLine("Invalid option.");
 		answer = string.Empty;
 		continue;
@@ -52,27 +50,8 @@ while (string.IsNullOrWhiteSpace(answer)) {
 }
 switch(answerNumber) {
 	case 2:
-		if (data.Days is null)
-			throw new Exception("You don't have any days recorded.");
-		var today = data.Days.FirstOrDefault(x => x.Date == DateOnly.FromDateTime(DateTime.Today));
-		if (today is null || today.TrackedSymptoms is null || !today.TrackedSymptoms.Any())
-			throw new Exception("No submission found for today");
-		Console.WriteLine("Here are your symptom submissions for today.");
-		double total = 0;
-		double weightSum = 0;
-		foreach(var t in today.TrackedSymptoms) {
-			var s = symptomMap[t.Id];
-			Console.WriteLine($"{s.Name}: {t.Value}");
-			double v = t.Value;
-			if (s.HigherBetter)
-				v = 10 - v;
-			v *= s.Weight;
-			total += v;
-			weightSum += s.Weight;
-		}
-		total /= weightSum;
-		total = Math.Round(total, 1);
-		Console.WriteLine($"Today, your overall score is {total}");
+		var today = DateOnly.FromDateTime(DateTime.Today);
+		generateReport(today);
 		break;
 	case 3: 
 		var notePath = Path.Combine(notesPath, $"{date}.md");
@@ -84,12 +63,45 @@ switch(answerNumber) {
 			}
 		}.Start();
 		break;
+	case 4:
+		Console.WriteLine($"Please enter the day (eg {date})");
+		var a = Console.ReadLine();
+		if (a is null)
+			throw new Exception("Error, please provide a date string");
+		var d = DateOnly.Parse(a);
+		generateReport(d);
+		break;
+}
+
+void generateReport(DateOnly d) {
+	if (data.Days is null)
+		throw new Exception("You don't have any days recorded.");
+	var day = data.Days.FirstOrDefault(x => x.Date == d);
+	if (day is null || day.TrackedSymptoms is null || !day.TrackedSymptoms.Any())
+		throw new Exception("No submission found for this day");
+	Console.WriteLine("Here are your symptom submissions.");
+	double total = 0;
+	double weightSum = 0;
+	foreach(var t in day.TrackedSymptoms) {
+		var s = symptomMap[t.Id];
+		Console.WriteLine($"{s.Name}: {t.Value}");
+		double v = t.Value;
+		if (!s.HigherBetter)
+			v = 10 - v;
+		v *= s.Weight;
+		total += v;
+		weightSum += s.Weight;
+	}
+	total /= weightSum;
+	total = Math.Round(total, 1);
+	Console.WriteLine($"Your overall score is {total}");
 }
 
 class Data {
 	required public string Name { get; set; }
 	required public List<Symptom> Symptoms { get; set; }
 	public List<Day>? Days { get; set; }
+	required public List<Action> Actions { get; set; }
 }
 
 class Symptom {
@@ -107,4 +119,9 @@ class Day {
 class TrackedSymptom {
 	required public string Id { get; set; }
 	required public int Value { get; set; }
+}
+
+class Action {
+	required public string Name { get; set; }
+	required public List<int> Scores { get; set; }
 }
